a7e83e6270f5ae0dfb35ae089bfea6e52ba0b176
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-01-01 11:09:54 -0200

    RobotsURL module

---
+++ robotsURL/robotsURL.go
@@ -0,0 +1,11 @@
+package gobotto
+
+import (
+	"net/url"
+)
+
+func RobotsURL(address string) (string, error) {
+	parsedUrl, err := url.Parse(address)
+	result := parsedUrl.Scheme + "://" + parsedUrl.Host + "/robots.txt"
+	return result, err
+}
---
+++ robotsURL/robotsURL_test.go
@@ -0,0 +1,14 @@
+package gobotto
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRobotsURLSuccessful(t *testing.T) {
+	expectedURL := "http://my-cool-domain.com/robots.txt"
+	result, err := RobotsURL("http://my-cool-domain.com/blog-post/1")
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedURL, result)
+}
