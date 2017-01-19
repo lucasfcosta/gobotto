a58ca9bb4873c05fb53250e1627cf5e32fd77026
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-01-19 18:48:58 -0200

    Documented go.doc file aggregating the other subpackages

--- doc.go
+++ doc.go
@@ -1,11 +1,34 @@
 /*
-Package gobotto offers a simple library for fetching, parsing and dealing with `robots.txt` files.
+  Copyright? Are you kidding? No rights reserved.
+  Use of this source code is totally free, do whatever you want with it.
+  I'm just giving something back to the internet, which has helped me with so many things in life I can't even count.
+  Thanks internet, you are by far mankind's greatest invention!
+*/
 
-Gobotto has the following methods:
+/*
+  Package gobotto offers a simple library for fetching, parsing and checking permissions against `robots.txt` files.
 
-	* gobotto.RobotsURL(URL string)
-	* gobotto.Fetch(URL string)
-	* gobotto.Parse(robotsContents string)
-	* gobotto.IsAllowed(userAgent string, route string, rules RobotsRules)
+  Gobotto has the following methods:
+
+   gobotto.RobotsURL(URL string)
+
+   gobotto.Fetch(URL string)
+
+   gobotto.Parse(robotsContents string)
+
+   gobotto.IsAllowed(userAgent string, route string, rules RobotsRules)
 */
 package gobotto
+
+import (
+	// models module
+	_ "github.com/lucasfcosta/gobotto/models"
+	// robotsurl module
+	_ "github.com/lucasfcosta/gobotto/robotsurl"
+	// fetch module
+	_ "github.com/lucasfcosta/gobotto/fetch"
+	// parse module
+	_ "github.com/lucasfcosta/gobotto/parse"
+	// isallowed module
+	_ "github.com/lucasfcosta/gobotto/isallowed"
+)
