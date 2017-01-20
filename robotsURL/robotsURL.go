e86d0bbb6e6d471f4e603a3e621c20d08bdb5549
lucasfcosta <fernandesdacostalucas@gmail.com>
2017-01-20 17:20:50 -0200

    Add summary and example for RobotsURL package

--- robotsURL/robotsURL.go
+++ robotsURL/robotsURL.go
@@ -1,3 +1,15 @@
+/*
+  Copy is not theft, do whatever you want with this code.
+  This is an example of how we can use some extra bytes for cool reflections instead of copyright stuff.
+    "The world's entire scientific and cultural heritage, published over centuries
+	in books and journals, is increasingly being digitized and locked up by a handful
+	of private corporations."
+	-SWARTZ, Aaron
+*/
+
+/*
+  Package robotsurl determines which is the URL to the robots.txt file for any site given any of its addresses.
+*/
 package robotsurl
 
 import (
--- robotsURL/robotsURL_test.go
+++ robotsURL/robotsURL_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+// Gets a robots.txt path for root urls
+func ExampleRobotsUrl_rootURL() {
+	url, err := RobotsURL("https://example.com/")
+	fmt.Println(url)
+	// Output: https://example.com/robots.txt
+}
+
+// Gets a robots.txt path for any urls
+func ExampleRobotsURL_anyUrl() {
+	url, err := RobotsURL("https://example.com/path/to/whatever")
+	fmt.Println(url)
+	// Output: https://example.com/robots.txt
+}
+
 func TestRobotsURLSuccessful(t *testing.T) {
 	expectedURL := "http://my-cool-domain.com/robots.txt"
 	result, err := RobotsURL("http://my-cool-domain.com/blog-post/1")
